6e427f75988ee2a5a45153587f8e5c90600f073f
Khushi Patel <khushiwonder@gmail.com>
2026-08-14 15:39:29 +0530

    Intialization of first next app

---
+++ my-first-next-app/src/app/page.js
@@ -0,0 +1,11 @@
+//localhost:3000/-> src/app
+
+export default function Home(){
+  //It always return JSX
+  //What is JSX?(jsXML = help to write html inside javascript)
+  return(
+    <main>
+      <h1>Hello World</h1>
+    </main>
+  )
+}
